d1943415ad40e22e6d24b3929ae80ba64150aec3
Mikko Haaramo <mikko.haaramo@alumni.aalto.fi>
2019-03-16 16:18:41 +0200

    1.3

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -5,47 +5,54 @@ const Header = ({ course }) => (
   <h1>{course}</h1>
 )
 
-const Part = ({ title, exercises }) => (
-  <p>{title} {exercises}</p>
+const Part = ({ name, exercises }) => (
+  <p>{name} {exercises}</p>
 )
 
 const Content = ({ 
-  exercises1,
-  exercises2,
-  exercises3,
   part1,
   part2,
   part3
 }) => (
   <>
-    <Part title={part1} exercises={exercises1} />
-    <Part title={part2} exercises={exercises2} />
-    <Part title={part3} exercises={exercises3} />
+    <Part {...part1} />
+    <Part {...part2} />
+    <Part {...part3} />
   </>
 )
 
-const Total = ({ exercises1, exercises2, exercises3 }) => (
-  <p>yhteensä {exercises1 + exercises2 + exercises3} tehtävää</p>
+const Total = ({ part1, part2, part3 }) => (
+  <p>yhteensä {part1.exercises + part2.exercises + part3.exercises} tehtävää</p>
 )
 
 const App = () => {
   const course = 'Half Stack -sovelluskehitys'
-  const part1 = 'Reactin perusteet'
-  const exercises1 = 10
-  const part2 = 'Tiedonvälitys propseilla'
-  const exercises2 = 7
-  const part3 = 'Komponenttien tila'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Reactin perusteet',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Tiedonvälitys propseilla',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'Komponenttien tila',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
       <Content
-        part1={part1} exercises1={exercises1}
-        part2={part2} exercises2={exercises2}
-        part3={part3} exercises3={exercises3}
+        part1={part1}
+        part2={part2}
+        part3={part3}
       />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Total
+        part1={part1}
+        part2={part2}
+        part3={part3}
+      />
     </div>
   )
 }
