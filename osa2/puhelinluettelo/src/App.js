74561d969fcf2136dfaf275df83dce126d6c3ec6
Mikko Haaramo <mikko.haaramo@alumni.aalto.fi>
2019-03-16 22:32:15 +0200

    2.8

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,27 +1,33 @@
 import React, { useState } from 'react'
 
-const Person = ({name}) => (
-  <div>{name}</div>
+const Person = ({name, number}) => (
+  <div>{name} {number}</div>
 )
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '045-123456' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
   const handleNameChange = event => setNewName(event.target.value)
+
+  const handleNumberChange = event => setNewNumber(event.target.value)
 
   const personExists = person => persons.findIndex(p => p.name === person.name) > -1
 
   const addPerson = (event) => {
-    const newPerson = { name: newName }
+    const newPerson = { name: newName, number: newNumber }
+
     event.preventDefault()
+
     if (personExists(newPerson)) {
       window.alert(`${newName} on jo luettelossa`)
     } else {
       setPersons(persons.concat(newPerson))
-      setNewName('')  
+      setNewName('')
+      setNewNumber('')
     }
   }
 
@@ -33,11 +39,14 @@ const App = () => {
           nimi: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          numero: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">lisää</button>
         </div>
       </form>
       <h2>Numerot</h2>
-      {persons.map(person => (<Person key={person.name} name={person.name} />))}
+      {persons.map(person => (<Person key={person.name} {...person} />))}
     </div>
   )
 
