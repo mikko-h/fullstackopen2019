b75f467dfd201b46d82568ec3c35c939d4070321
Mikko Haaramo <mikko.haaramo@alumni.aalto.fi>
2019-03-16 21:42:55 +0200

    2.4

--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -11,7 +11,7 @@ const Part = ({name, exercises}) => (
 
 const Content = ({parts}) => (
   <>
-    {parts.map(part => (<Part key={part.name} {...part} />))}
+    {parts.map(part => (<Part key={part.id} {...part} />))}
   </>
 )
 
@@ -29,31 +29,53 @@ const Course = ({course}) => (
     <Total parts={course.parts} />
   </div>
 )
+
 const App = () => {
-  const course = {
-    name: 'Half Stack -sovelluskehitys',
-    parts: [
-      {
-        name: 'Reactin perusteet',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Tiedonvälitys propseilla',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'Komponenttien tila',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack -sovelluskehitys',
+      id: 1,
+      parts: [
+        {
+          name: 'Reactin perusteet',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Tiedonvälitys propseilla',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'Komponenttien tila',
+          exercises: 14,
+          id: 3
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewaret',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
 
   return (
     <div>
-      <Course course={course} />
+      <h1>Opetusohjelma</h1>
+      {courses.map(course => (<Course key={course.id} course={course} />))}
     </div>
   )
 }
