0a27153ee5eb60ee5210c183c1686514d50a651b
Mikko Haaramo <mikko.haaramo@alumni.aalto.fi>
2019-03-16 22:43:30 +0200

    2.9

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -6,16 +6,24 @@ const Person = ({name, number}) => (
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '045-123456' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Martti Tienari', number: '040-123456' },
+    { name: 'Arto Järvinen', number: '040-123456' },
+    { name: 'Lea Kutvonen', number: '040-123456' }
   ]) 
+  const [ nameFilter, setNameFilter ] = useState('')
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+
+  const handleFilterChange = event => setNameFilter(event.target.value)
 
   const handleNameChange = event => setNewName(event.target.value)
 
   const handleNumberChange = event => setNewNumber(event.target.value)
 
   const personExists = person => persons.findIndex(p => p.name === person.name) > -1
+
+  const personFilter = person => person.name.toLowerCase().indexOf(nameFilter.toLowerCase()) > -1
 
   const addPerson = (event) => {
     const newPerson = { name: newName, number: newNumber }
@@ -33,7 +41,11 @@ const App = () => {
 
   return (
     <div>
-      <h2>Puhelinluettelo</h2>
+      <h1>Puhelinluettelo</h1>
+      <div>
+        rajaa näytettäviä <input onChange={handleFilterChange} />
+      </div>
+      <h2>Lisää uusi</h2>
       <form onSubmit={addPerson}>
         <div>
           nimi: <input value={newName} onChange={handleNameChange} />
@@ -46,7 +58,9 @@ const App = () => {
         </div>
       </form>
       <h2>Numerot</h2>
-      {persons.map(person => (<Person key={person.name} {...person} />))}
+      {persons
+        .filter(personFilter)
+        .map(person => (<Person key={person.name} {...person} />))}
     </div>
   )
 
