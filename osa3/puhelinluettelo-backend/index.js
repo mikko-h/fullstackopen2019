bac71eb9f235d0018d42153b2819b660bdf291e4
Mikko Haaramo <mikko.haaramo@alumni.aalto.fi>
2019-04-20 19:46:35 +0300

    3.4

--- osa3/puhelinluettelo-backend/index.js
+++ osa3/puhelinluettelo-backend/index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const persons = require('./db.json')
+let persons = require('./db.json')
 
 const app = express()
 
@@ -16,6 +16,13 @@ app.get('/api/persons/:id', (req, res) => {
   } else {
     res.status(404).end()
   }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(p => p.id !== id)
+
+  res.status(204).end()
 })
 
 app.get('/info', (req, res) => {
