0299431ade5961cf84884fad619257dd0471b6b5
Mikko Haaramo <mikko.haaramo@alumni.aalto.fi>
2019-04-20 19:40:08 +0300

    3.1

---
+++ osa3/puhelinluettelo-backend/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const persons = require('./db.json')
+
+const app = express()
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
