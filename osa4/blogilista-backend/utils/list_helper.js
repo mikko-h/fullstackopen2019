1a3a208ce28149a28f7bed1e420f84fa497f4dd8
Mikko Haaramo <mikko.haaramo@alumni.aalto.fi>
2019-04-25 20:11:03 +0300

    4.5

--- osa4/blogilista-backend/utils/list_helper.js
+++ osa4/blogilista-backend/utils/list_helper.js
@@ -1,8 +1,14 @@
 const dummy = () => 1
 
+const favoriteBlog = blogs => blogs.reduce(
+  (favorite, blog) => ((favorite && favorite.likes >= blog.likes) ? favorite : blog),
+  undefined
+)
+
 const totalLikes = blogs => blogs.reduce((sum, blog) => sum + blog.likes, 0)
 
 module.exports = {
   dummy,
+  favoriteBlog,
   totalLikes
 }
