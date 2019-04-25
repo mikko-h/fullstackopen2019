83f908aec28ca7cd8d8b3ceb60c875d78ed44432
Mikko Haaramo <mikko.haaramo@alumni.aalto.fi>
2019-04-25 16:42:59 +0300

    4.1

---
+++ osa4/blogilista-backend/.eslintrc.js
@@ -0,0 +1,51 @@
+module.exports = {
+  'env': {
+    'commonjs': true,
+    'es6': true,
+    'node': true
+  },
+  'extends': 'eslint:recommended',
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly'
+  },
+  'parserOptions': {
+    'ecmaVersion': 2018
+  },
+  'rules': {
+    'arrow-spacing': [
+      'error', {
+        'before': true,
+        'after': true
+      }
+    ],
+    'eqeqeq': 'error',
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'no-console': 0,
+    'no-trailing-spaces': 'error',
+    'no-unused-vars': [
+      'error',
+      {
+        'ignoreRestSiblings': true
+      }
+    ],
+    'object-curly-spacing': [
+      'error', 'always'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ]
+  }
+}
