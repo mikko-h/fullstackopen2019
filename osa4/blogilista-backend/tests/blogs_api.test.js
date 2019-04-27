d7e49a130df9a05323376ce8eed4fef1690867c5
Mikko Haaramo <mikko.haaramo@alumni.aalto.fi>
2019-04-27 13:44:22 +0300

    4.13

--- osa4/blogilista-backend/tests/blogs_api.test.js
+++ osa4/blogilista-backend/tests/blogs_api.test.js
@@ -93,6 +93,21 @@ describe('when there is initially some blogs saved', () => {
       .send(newBlog)
       .expect(400)
   })
+
+  test('a blog can be deleted', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
+
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd.length).toBe(helper.initialBlogs.length - 1)
+
+    const titles = blogsAtEnd.map(b => b.title)
+    expect(titles).not.toContain(blogToDelete.title)
+  })
 })
 
 afterAll(() => {
