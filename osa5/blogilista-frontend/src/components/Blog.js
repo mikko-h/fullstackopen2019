ec11b0bd2bc4eaf0d0c7a577bc10beed457e5b90
Mikko Haaramo <mikko.haaramo@alumni.aalto.fi>
2019-05-05 12:19:36 +0300

    5.6

--- osa5/blogilista-frontend/src/components/Blog.js
+++ osa5/blogilista-frontend/src/components/Blog.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const expandStyle = {
+    cursor: 'pointer'
+  }
+
+  const [expanded, setExpanded] = useState(false)
+
+  const blogDetails = () => (
+    <div>
+      <a href={blog.url}>{blog.url}</a>
+      <div>{blog.likes} likes <button>like</button></div>
+      {blog.user && <div>added by {blog.user.name}</div>}
+    </div>
+  )
+
+  return (
+    <div style={blogStyle}>
+      <div style={expandStyle} onClick={() => setExpanded(!expanded)}>{blog.title} {blog.author}</div>
+      {expanded && blogDetails()}
+    </div>
+  )
+}
 
 export default Blog
