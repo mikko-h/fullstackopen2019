55dc95d75c2824ad024616d1ad1139b616e20ea6
Mikko Haaramo <mikko.haaramo@alumni.aalto.fi>
2019-12-29 22:17:05 +0200

    7.2

--- osa7/routed-anecdotes/src/App.js
+++ osa7/routed-anecdotes/src/App.js
@@ -22,8 +22,18 @@ const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      {anecdotes.map(anecdote =>
+        <li key={anecdote.id}><Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link></li>)
+      }
     </ul>
+  </div>
+)
+
+const Anecdote = ({ anecdote }) => (
+  <div>
+    <h2>{anecdote.content} by {anecdote.author}</h2>
+    <p>has {anecdote.votes} votes</p>
+    <p>for more info see <a href={anecdote.info}>{anecdote.info}</a></p>
   </div>
 )
 
@@ -134,6 +144,9 @@ const App = () => {
         <Menu />
         <Route exact path="/" render={() =>
           <AnecdoteList anecdotes={anecdotes} />
+        } />
+        <Route exact path="/anecdotes/:id" render={({ match }) =>
+          <Anecdote anecdote={anecdoteById(match.params.id)} />
         } />
         <Route path="/about" render={() =>
           <About />
