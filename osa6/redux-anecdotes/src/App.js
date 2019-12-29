11c7e24f240cbfe7703caa277ffbd7439b55d0be
Mikko Haaramo <mikko.haaramo@alumni.aalto.fi>
2019-12-29 13:37:37 +0200

    6.16

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
+import anecdoteService from './services/anecdotes'
+import { initAnecdotes } from './reducers/anecdoteReducer'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Notification from './components/Notification'
 
-const App = () => (
-  <div>
-    <h1>Programming anecdotes</h1>
-    <Notification />
-    <AnecdoteForm />
-    <AnecdoteList />
-  </div>
-)
+const App = (props) => {
+  useEffect(() => {
+    anecdoteService
+      .getAll().then(anecdotes => props.initAnecdotes(anecdotes))
+  })
 
-export default App
+  return (
+    <div>
+      <h1>Programming anecdotes</h1>
+      <Notification />
+      <AnecdoteForm />
+      <AnecdoteList />
+    </div>
+  )
+}
+
+export default connect(null, { initAnecdotes })(App)
--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,12 +1,3 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
 const getId = () => (100000 * Math.random()).toFixed(0)
 
 const asObject = (anecdote) => {
@@ -17,12 +8,12 @@ const asObject = (anecdote) => {
   }
 }
 
-const initialState = anecdotesAtStart.map(asObject)
-
 const sorted = anecdotes => anecdotes.sort((a, b) => b.votes - a.votes)
 
-const anecdoteReducer = (state = initialState, action) => {
+const anecdoteReducer = (state = [], action) => {
   switch (action.type) {
+    case 'INIT_ANECDOTES':
+      return sorted(action.data)
     case 'NEW_ANECDOTE':
       return sorted(state.concat(action.data))
     case 'VOTE':
@@ -31,6 +22,11 @@ const anecdoteReducer = (state = initialState, action) => {
       return state
   }
 }
+
+export const initAnecdotes = (anecdotes) => ({
+  type: 'INIT_ANECDOTES',
+  data: anecdotes
+})
 
 export const voteAnecdote = (id) => ({
   type: 'VOTE',
